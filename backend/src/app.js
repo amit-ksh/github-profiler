a7d17b19ee6f39f86ad92d17a7ed0c92410ca2d2
amit-ksh <ksamit1110@gmail.com>
2023-01-15 15:25:05 +0530

    deploy setup and test: error fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(morgan('dev'));
 app.use(express.json());
 
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,23 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private url = 'localhost:4500';
+  private url = environment.API_URL;
 
   constructor(private httpClient: HttpClient) {}
 
-  user: any = null;
 
   getUser(username: string) {
-    return this.httpClient.get(`http://${this.url}/${username}`);
+    return this.httpClient.get(`${this.url}/${username}`);
   }
 
   getRepos(username: string, page: number = 1) {
     return this.httpClient.get(
-      `http://${this.url}/${username}/repos?page=${page}`
+      `${this.url}/${username}/repos?page=${page}`
     );
   }
 }
